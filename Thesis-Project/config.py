5bf850c1276cac70745f31870c6807a4fcce173b
Sonia Matei <soniam26602@gmail.com>
2024-04-09 00:03:32 +0300

    model schema and aug val config, no masks in collade_fn

--- Thesis-Project/config.py
+++ Thesis-Project/config.py
@@ -1,6 +1,7 @@
 import pathlib
-from torchvision.transforms import v2
+
 import torch
+from torchvision.transforms import v2
 
 ROOT_DIR = pathlib.Path("/home/sonia/Documents/Licenta/Thesis-Project/Thesis-Project")
 BASIC_TRANSFORMS = v2.Compose(
@@ -9,3 +10,98 @@ BASIC_TRANSFORMS = v2.Compose(
         v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)]),
     ]
 )
+
+MODEL_SETTINGS_SCHEMA = {
+    "type": "object",
+    "properties": {
+        "resnet_lstm_features": {
+            "type": "integer"
+        },
+        "lstm_hidden_size": {
+            "type": "integer"
+        },
+        "lstm_num_layers": {
+            "type": "integer"
+        },
+        "fc1_features": {
+            "type": "integer"
+        },
+        "fc2_features": {
+            "type": "integer"
+        },
+        "final_output_features": {
+            "type": "integer"
+        }
+    },
+    "required": [
+        "resnet_lstm_features",
+        "lstm_hidden_size",
+        "lstm_num_layers",
+        "fc1_features",
+        "fc2_features",
+        "final_output_features"
+    ]
+}
+
+AUGMENTATION_VALUES_SCHEMA = {
+    "type": "object",
+    "properties": {
+        "resize": {
+            "type": "array",
+            "items": {
+                "type": "integer"
+            },
+            "minItems": 2,
+            "maxItems": 2
+        },
+        "horizontal_flip": {
+            "type": "number",
+        },
+        "vertical_flip": {
+            "type": "number",
+        },
+        "rotation": {
+            "type": "array",
+            "items": {
+                "type": "integer"
+            },
+            "minItems": 2,
+            "maxItems": 2
+        },
+        "color_jitter_params": {
+            "type": "object",
+            "properties": {
+                "brightness": {
+                    "type": "number",
+                },
+                "contrast": {
+                    "type": "number",
+                },
+                "saturation": {
+                    "type": "number",
+                },
+                "hue": {
+                    "type": "number",
+                }
+            },
+            "required": ["brightness", "contrast", "saturation", "hue"]
+        },
+        "mean": {
+            "type": "array",
+            "items": {
+                "type": "number"
+            },
+            "minItems": 3,
+            "maxItems": 3
+        },
+        "std": {
+            "type": "array",
+            "items": {
+                "type": "number"
+            },
+            "minItems": 3,
+            "maxItems": 3
+        }
+    },
+    "required": ["resize", "horizontal_flip", "vertical_flip", "rotation", "color_jitter_params", "mean", "std"]
+}
--- Thesis-Project/utils.py
+++ Thesis-Project/utils.py
@@ -1,4 +1,5 @@
 import torch
+
 from data_handling.video_dataset import VideoDataset
 
 
@@ -6,23 +7,21 @@ def collate_fn_pad(batch: list[tuple[torch.Tensor]]) -> tuple[torch.Tensor]:
     """
     Pads batches of variable length. Dataloader helper function.
     :param batch: list of returned tuples from dataset as (frames, label)
-    :return: the padded videos, the corresponding labels, the lengths of each sequence in the batch,
-             and the mask indicating the padded elements
+    :return: - the padded videos [frames, channels, height, width]
+             - the corresponding labels [label]
+             - the lengths of each sequence in the batch (no. frames) [length]
     """
     # separate videos and labels
-    video_list, labels = zip(*batch)
+    videos, labels = zip(*batch)
 
     # get number of frames
-    lengths = torch.tensor([t.size(0) for t in video_list], dtype=torch.int)
+    lengths = torch.tensor([t.size(0) for t in videos], dtype=torch.int)
 
     # pad after max length tensor
-    video_list = torch.nn.utils.rnn.pad_sequence(video_list, batch_first=True)
-    labels = torch.tensor(labels, dtype=torch.int)
+    videos = torch.nn.utils.rnn.pad_sequence(videos, batch_first=True)
+    labels = torch.tensor(labels, dtype=torch.float32)
 
-    # mask to ignore padded data
-    mask = (video_list != 0).float()
-
-    return video_list, labels, lengths, mask
+    return videos, labels, lengths
 
 
 def compute_mean_std(
@@ -32,7 +31,7 @@ def compute_mean_std(
     Calculate mean and std for a dataset.
     :param dataset: the desired dataset
     :param device: option for calc. for optimization. defaults to 'cpu'
-    :return: the calculated mean and standard deviation
+    :return: the calculated mean and standard deviation [channel_1, channel_2, channel_3]
     """
     if device not in ["cpu", "cuda"]:
         raise ValueError(f"Device should be 'cpu' or 'cuda', got {device}.")
@@ -46,7 +45,7 @@ def compute_mean_std(
         for batch in dataset:
             video, _ = batch
             video = video.to(device)
-            # make [channels, batch, frames, height, width]
+            # make [channels, frames, height, width]
             video = video.permute(1, 0, 2, 3)
 
             batch_pixel_count = video.size(1) * video.size(2) * video.size(3)
